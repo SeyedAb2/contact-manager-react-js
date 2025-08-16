32e9440eb69570a5ed34f635dcc1d3764ce601e3
a-musavi <tiggerwitht1378@gmail.com>
2025-08-17 00:14:34 +0330

    refactor api path

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = 'https://json-server-contact-react-js.vercel.app/api/'
+const SERVER_URL = 'https://json-server-contact-react-js.vercel.app/api'
 
 //  contacts
 export const getAllContact = () =>{
